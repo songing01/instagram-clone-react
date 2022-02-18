3dae8c5a8efa558ada609cc22a9e4021130a3158
songing01 <79829662+songing01@users.noreply.github.com>
2022-02-19 01:22:01 +0900

    Design: create app_header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app_header">
+        <img
+          calssName="app_headerImage"
+          src="https://fontmeme.com/images/instagram-new-logo.png"
+          width={"100rem"}
+          alt=""
+        ></img>
+      </div>
+      <h1>Hello</h1>
+      {/*Header*/}
+
+      {/*posts */}
+      {/*posts */}
     </div>
   );
 }
