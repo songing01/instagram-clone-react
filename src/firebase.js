4e6ed8673d4d4292d9cc55365458d04babdac82a
songing01 <79829662+songing01@users.noreply.github.com>
2022-03-02 14:33:41 +0900

    Feat: Implement firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDvCMSE8tGxj9KnT5DYx17Ishi4eBd0MMs",
+  authDomain: "instagram-clone-react-2ab25.firebaseapp.com",
+  databaseURL:
+    "https://instagram-clone-react-2ab25-default-rtdb.firebaseio.com",
+  projectId: "instagram-clone-react-2ab25",
+  storageBucket: "instagram-clone-react-2ab25.appspot.com",
+  messagingSenderId: "993000959278",
+  appId: "1:993000959278:web:b9c3f595d52fc1dc511ebf",
+  measurementId: "G-DKKNPB85TC",
+});
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+0000;
+export { db, auth, storage };
